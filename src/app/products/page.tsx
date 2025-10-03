8c7b4bfeda4f2d8d5fee8b01d1935ec68cb2c195
sandeep-barala <sandeep.barala@teaminindia.com>
2025-10-03 12:38:36 +0530

    Initial commint for Next.js project

---
+++ src/app/products/page.tsx
@@ -0,0 +1,8 @@
+export default function ProductsPage() {    
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <p>Explore our range of products designed to meet your needs and enhance your experience.</p>
+    </div>
+  );
+}
